2558c880de6394313ad66d5e4aede5afdc99363c
One_Ordinary_Man <srivathsansudarsanan@outlook.com>
2023-08-07 21:46:42 +0530

    Added the source code

--- src/script.bash
+++ src/script.bash
@@ -1,2 +1,14 @@
 #!/usr/bin/bash
-echo "Hello, World!"
+media_progs="Kodi\nMusic\nCalibre"
+choice=$(echo -e "$media_progs" | rofi -dmenu -i -p "Execute:-")
+
+case $choice in
+    Kodi)
+        kodi
+        ;;
+    Music)
+        wezterm start --always-new-process "ncmpcpp"
+        ;;
+    Calibre)
+        calibre
+esac
